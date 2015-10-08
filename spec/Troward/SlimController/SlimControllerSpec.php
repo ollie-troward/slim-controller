02f9f106a96dd1e9b28ef91a88f4272c1265e6b3
Ollie Troward <ollie@ollietroward.com>
2015-10-08 20:05:17 +0100

    Accepting routes and namespace configuration

--- spec/Troward/SlimController/SlimControllerSpec.php
+++ spec/Troward/SlimController/SlimControllerSpec.php
@@ -4,11 +4,70 @@ namespace spec\Troward\SlimController;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Slim\Slim;
 
 class SlimControllerSpec extends ObjectBehavior
 {
     function it_is_initializable()
     {
         $this->shouldHaveType('Troward\SlimController\SlimController');
+    }
+
+    function let()
+    {
+        $app = new Slim;
+        $this->beConstructedWith($app);
+    }
+
+    function it_returns_an_application_namespace()
+    {
+        $app = new Slim;
+        $config = ['namespace' => 'MyApp\\Controllers\\'];
+
+        $this->beConstructedWith($app, $config);
+
+        $this->getConfig('namespace')
+            ->shouldReturn($config['namespace']);
+    }
+
+    function it_accepts_an_array_of_valid_routes()
+    {
+        $routes = [
+            'RESOURCE' => [
+                'user' => 'UserController'
+            ],
+            'GET' => [
+                '/' => 'UserController@index'
+            ],
+            'POST' => [
+                '/' => 'UserController@create'
+            ],
+            'PUT' => [
+                '/' => 'UserController@update'
+            ],
+            'DELETE' => [
+                '/' => 'UserController@delete'
+            ],
+        ];
+
+        $this->routes($routes);
+
+        $this->getRoutes()
+            ->shouldReturn($routes);
+    }
+
+    function it_disallows_an_array_with_an_invalid_method()
+    {
+        $routes = [
+            'RESOURCE' => [
+                'user' => 'UserController'
+            ],
+            'EAT' => [
+                '/' => 'UserController@index'
+            ],
+        ];
+
+        $this->shouldThrow('\InvalidArgumentException')
+            ->duringRoutes($routes);
     }
 }
--- src/Troward/SlimController/SlimController.php
+++ src/Troward/SlimController/SlimController.php
@@ -2,7 +2,82 @@
 
 namespace Troward\SlimController;
 
+use Slim\Slim;
+
+/**
+ * Class SlimController
+ * @package Troward\SlimController
+ */
 class SlimController
 {
+    /**
+     * @var Slim
+     */
+    protected $app;
 
+    /**
+     * @var array
+     */
+    private $config;
+
+    /**
+     * @var array
+     */
+    private $routes;
+
+    /**
+     * @var array
+     */
+    private $routeMethods = ['GET', 'POST', 'PUT', 'DELETE', 'RESOURCE'];
+
+    /**
+     * @param Slim $app
+     * @param array $config
+     */
+    public function __construct(Slim $app, $config = [])
+    {
+        $this->app = $app;
+        $this->config = $config;
+    }
+
+    /**
+     * @param string $key
+     * @return array|string
+     * @throws \InvalidArgumentException
+     */
+    public function getConfig($key = null)
+    {
+        if (is_null($key)) {
+            return $this->config;
+        }
+
+        if (!isset($this->config[$key])) {
+            throw new \InvalidArgumentException('Invalid configuration option: ' . $key);
+        }
+
+        return $this->config[$key];
+    }
+
+    /**
+     * @return array
+     */
+    public function getRoutes()
+    {
+        return $this->routes;
+    }
+
+    /**
+     * @param array $routes
+     * @throws \InvalidArgumentException
+     */
+    public function routes($routes = [])
+    {
+        foreach ($routes as $method => $route) {
+            if (!in_array($method, $this->routeMethods)) {
+                throw new \InvalidArgumentException('Invalid route method: ' . $method);
+            }
+        }
+
+        $this->routes = $routes;
+    }
 }
