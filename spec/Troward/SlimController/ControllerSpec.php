cce466adab979a03ce6289b58c6e614f1b19b1a6
Ollie Troward <ollie@ollietroward.com>
2015-10-09 09:20:46 +0100

    Accepts an instance of Slim

--- spec/Troward/SlimController/ControllerSpec.php
+++ spec/Troward/SlimController/ControllerSpec.php
@@ -4,11 +4,28 @@ namespace spec\Troward\SlimController;
 
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Slim\Slim;
 
+/**
+ * Class ControllerSpec
+ * @package spec\Troward\SlimController
+ */
 class ControllerSpec extends ObjectBehavior
 {
+    /**
+     * It can be initialized.
+     */
     function it_is_initializable()
     {
         $this->shouldHaveType('Troward\SlimController\Controller');
+    }
+
+    /**
+     * It accepts an instance of Slim.
+     */
+    function it_accepts_an_app_instance()
+    {
+        $app = new Slim;
+        $this->setApp($app);
     }
 }
