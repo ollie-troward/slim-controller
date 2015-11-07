f04596314b725872b606462c9de207ed40650260
Ollie Troward <ollie@ollietroward.com>
2015-11-07 19:49:02 +0000

    SlimController refactor, including the feature of reusing the same controller instances

--- src/Troward/SlimController/SlimController.php
+++ src/Troward/SlimController/SlimController.php
@@ -39,14 +39,21 @@ class SlimController
     private $routeMethods = ['GET', 'POST', 'PUT', 'DELETE', 'RESOURCE'];
 
     /**
+     * Initialised controllers.
+     *
+     * @var array
+     */
+    private $controllers = [];
+
+    /**
      * SlimController constructor.
      *
-     * @param Slim  $app
+     * @param Slim $app
      * @param array $config
      */
     public function __construct(Slim $app, $config = [])
     {
-        $this->app    = $app;
+        $this->app = $app;
         $this->config = $config;
     }
 
@@ -89,63 +96,111 @@ class SlimController
      */
     public function routes($routes = [])
     {
-        foreach ($routes as $method => $route) {
-            if (!$this->isValidRoute($method)) {
-                throw new \InvalidArgumentException('Invalid route method: ' . $method);
+        foreach ($routes as $httpVerb => $route) {
+            $this->validateHttpVerb($httpVerb);
+
+            foreach ($route as $uri => $route) {
+                $route = $this->splitRoute($route);
+
+                $controller = $this->retrieveController($route['controller']);
+
+                $controller->setApp($this->app);
+
+                $this->execute($controller, $route['method'], $httpVerb, $uri);
             }
 
-            foreach ($route as $uri => $target) {
-                $targetParts = explode('@', $target);
-
-                if (!$this->isValidControllerStructure($targetParts)) {
-                    throw new \InvalidArgumentException('Invalid controller structure: ' . $target);
-                }
-
-                $controller = $targetParts[0];
-                $function   = $targetParts[1];
-
-                if (isset($this->config['namespace'])) {
-                    $controller = $this->config['namespace'] . $controller;
-                }
-
-                if (!class_exists($controller)) {
-                    throw new \InvalidArgumentException('Invalid controller specified: ' . $controller);
-                }
-
-                $baseController = new $controller;
-                $baseController->setApp($this->app);
-
-                $callBack = [$baseController, $function];
-
-                if (!is_callable($callBack)) {
-                    throw new \InvalidArgumentException($controller . '::' . $function . ' is not callable');
-                }
-
-                $this->app->$method($uri, $callBack);
-            }
+            $this->routes = $routes;
         }
-        $this->routes = $routes;
     }
 
     /**
-     * Validate the route.
+     * Validate the HTTP Verb.
      *
-     * @param $method
-     * @return bool
+     * @param string $httpVerb
+     * @return void
+     * @throws \InvalidArgumentException
      */
-    private function isValidRoute($method)
+    private function validateHttpVerb($httpVerb)
     {
-        return in_array($method, $this->routeMethods);
+        if (!in_array($httpVerb, $this->routeMethods)) {
+            throw new \InvalidArgumentException('Invalid route method: ' . $httpVerb);
+        }
     }
 
     /**
-     * Validate the controller and function structure.
+     * Split and validate the route structure.
      *
-     * @param array $targetParts
-     * @return bool
+     * @param string $route
+     * @return array
+     * @throws \InvalidArgumentException
      */
-    private function isValidControllerStructure(array $targetParts)
+    private function splitRoute($route)
     {
-        return count($targetParts) == 2;
+        $routeParts = explode('@', $route);
+
+        if (count($routeParts) !== 2) {
+            throw new \InvalidArgumentException('Invalid controller structure: ' . $route);
+        }
+
+        return [
+            'controller' => $routeParts[0],
+            'method' => $routeParts[1],
+        ];
+    }
+
+    /**
+     * Append the namespace if specified.
+     * Check the class exists.
+     * Check that we haven't already stored the controller from earlier.
+     * Check that it extends from the correct Controller.
+     * Return a instance of the controller.
+     *
+     * @param string $controller
+     * @return mixed
+     * @throws \InvalidArgumentException
+     * @throws \BadMethodCallException
+     */
+    private function retrieveController($controller)
+    {
+        if (isset($this->config['namespace'])) {
+            $controller = $this->config['namespace'] . $controller;
+        }
+
+        if (!class_exists($controller)) {
+            throw new \InvalidArgumentException('Invalid controller specified: ' . $controller);
+        }
+
+        if (array_key_exists($controller, $this->controllers)) {
+            return $this->controllers[$controller];
+        }
+
+        $this->controllers[$controller] = new $controller;
+
+        if (!is_subclass_of($controller, Controller::class)) {
+            throw new \BadMethodCallException(get_class($controller) . ' does not extend: ' . Controller::class);
+        }
+
+        return $this->controllers[$controller];
+    }
+
+    /**
+     * Execute the route.
+     *
+     * @param ControllerInterface $controller
+     * @param string $method
+     * @param string $httpVerb
+     * @param string $route
+     * @return void
+     * @throws \InvalidArgumentException
+     */
+    private function execute(ControllerInterface $controller, $method, $httpVerb, $route)
+    {
+        $callBack = [$controller, $method];
+
+        if (!is_callable($callBack)) {
+            throw new \InvalidArgumentException(get_class($controller) . '::' . $method . ' is not callable');
+        }
+
+        $this->app->$httpVerb($route, $callBack);
     }
 }
